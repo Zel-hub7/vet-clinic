90fb0721cb5d222e637bf9c8e9c9335162d49ca5
Zelalem Yohannes <zelalemyohannes75@gmail.com>
2023-09-19 11:29:04 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
-schema
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(8, 2)
+);
