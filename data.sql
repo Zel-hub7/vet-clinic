d182622bf406c00ebd77bc3322669d9013ab623e
Zelalem Yohannes <zelalemyohannes75@gmail.com>
2023-09-20 10:39:44 -0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,15 @@
-data
+-- Insert data for Agumon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Agumon', '2020-02-03', 0, true, 10.23);
+
+-- Insert data for Gabumon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Gabumon', '2018-11-15', 2, true, 8);
+
+-- Insert data for Pikachu
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
+
+-- Insert data for Devimon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Devimon', '2017-05-12', 5, true, 11);
