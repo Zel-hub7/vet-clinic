ff7ff06edbaad9c29d3011c18a10540bc0c7d139
Zelalem Yohannes <zelalemyohannes75@gmail.com>
2023-09-21 05:49:22 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -38,3 +38,50 @@ ROLLBACK;
 --Check if the table become as it was before the changes were made.
 
 SELECT * FROM animals;
+
+--Update the animals table by setting the species column to digimon for all animals that have a name ending in mon
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+--Verify that changes were made.
+SELECT * FROM animals;
+
+--COMMIT CHANGES
+
+COMMIT;
+
+--VERIFY IF THE CHANGES ARE PERMANENT
+
+SELECT * FROM animals;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
