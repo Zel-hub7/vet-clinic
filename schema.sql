9d26bdddd21f1970684695ee53a0b971784eb9d3
Zelalem Yohannes <zelalemyohannes75@gmail.com>
2023-09-23 16:53:23 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,72 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(255);
+
+
+-- Create a table named owners
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name TEXT,
+    age INTEGER
+);
+
+
+-- Create table named species.
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+
+-- Make sure that id is set as autoincremented PRIMARY KEY
+
+-- CREATE new Table named temp_animals;
+CREATE TABLE temp_animals (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(5, 2),
+    species TEXT
+);
+
+
+-- Copy all the datas from the animals table to temp_animals;
+
+INSERT INTO temp_animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+SELECT name, date_of_birth, escape_attempts, neutered, weight_kg, species
+FROM animals;
+
+
+-- Drom the existing animals table
+
+DROP animals;
+
+-- Rename the temp_animals table to animals.
+
+ALTER TABLE temp_animals RENAME TO animals;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
