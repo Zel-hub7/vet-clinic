0b3a0915866da0a8d8b1b050e7b0ee679b6feafb
Zelalem Yohannes <zelalemyohannes75@gmail.com>
2023-09-24 12:34:27 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -26,3 +26,31 @@ VALUES
   ('Boarmon', '2005-06-07', 7, true, 20.4),
   ('Blossom', '1998-10-13', 3, true, 17),
   ('Ditto', '2022-05-14', 4, true, 22);
+
+-- Insert Data into owners table
+
+INSERT INTO owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+-- Insert Data into species table;
+
+INSERT INTO species (name)
+VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id values;
+
+UPDATE animals
+SET species_id = (
+    CASE
+        WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+        ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+      END
+);
