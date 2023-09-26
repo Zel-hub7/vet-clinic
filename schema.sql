f84b084a41c6bf7dbba8d5bc8c0a766356dc4eeb
Zelalem Yohannes <zelalemyohannes75@gmail.com>
2023-09-26 11:07:21 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -86,6 +86,35 @@ ADD CONSTRAINT fk_owner
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
 
+--Create a table named vets
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INTEGER,
+    date_of_graduation DATE
+);
+
+-- Create a "join table" called specializations
+
+CREATE TABLE specializations (
+    vet_id INTEGER,
+    species_id INTEGER,
+    PRIMARY KEY (vet_id, species_id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id),
+    FOREIGN KEY (species_id) REFERENCES species (id)
+);
+
+-- Create a "join table" called visits
+
+CREATE TABLE visits (
+    animal_id INTEGER,
+    vet_id INTEGER,
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date),
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
 
 
 
